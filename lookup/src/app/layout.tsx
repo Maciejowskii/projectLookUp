a7038eb80a2524a1fb5267047a6676ede3808206
Maciej Tyra <maciejtyra55@gmail.com>
2026-01-15 23:20:21 +0100

    fix

--- lookup/src/app/layout.tsx
+++ lookup/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { prisma } from '@/lib/prisma'
 import { Toaster } from 'react-hot-toast'
-import { GoogleTagManager } from '@next/third-parties/google'
+// GoogleTagManager removed - using direct script implementation for better compatibility
 import { Providers } from '@/components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -55,21 +55,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<head>
 				{/* JSON-LD Schema: To jest kluczowe dla Google Site Name */}
 				<script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+
+				{/* Google Tag Manager - Direct implementation for better compatibility */}
+				{gtmId && (
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');
+							`.trim(),
+						}}
+					/>
+				)}
+				{/* End Google Tag Manager */}
 			</head>
 			<body className={inter.className}>
 				{/* Google Tag Manager (noscript) - must be immediately after opening <body> tag */}
-				<noscript>
-					<iframe
-						src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-						height='0'
-						width='0'
-						style={{ display: 'none', visibility: 'hidden' }}
-					/>
-				</noscript>
+				{gtmId && (
+					<noscript>
+						<iframe
+							src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+							height='0'
+							width='0'
+							style={{ display: 'none', visibility: 'hidden' }}
+						/>
+					</noscript>
+				)}
 				{/* End Google Tag Manager (noscript) */}
-
-				{/* GoogleTagManager component automatically adds script to <head> */}
-				{gtmId && <GoogleTagManager gtmId={gtmId} />}
 
 				<Providers>
 					{children}
